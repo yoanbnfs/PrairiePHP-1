9a58a3e28a9a71ad2fb603aa7f48bdd9c5927dfa
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-10-02 10:36:22 +0200

    affiche les calculs

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  $chiffre = 4;
+  $chiffre1 = 8;
+  $addition = $chiffre+$chiffre1;
+  $multiplication = $chiffre*$chiffre1;
+  echo $chiffre . ' + ' . $chiffre1 . ' = ' . $addition;
+  echo $chiffre . ' x ' . $chiffre1 . ' = ' . $multiplication;
+ ?>
